bea77279d9dc8ddd8b14070cdb7900e6e8f3b1d3
DM-Coder-2007 <debansumondal2304@gmail.com>
2026-09-09 22:02:20 +0530

    sixth push

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -4,6 +4,7 @@ initOpenNextCloudflareForDev();
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output:"export",
   reactStrictMode: true,
   compress: true,
   poweredByHeader: false,
